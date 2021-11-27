29e1486a9ccbfc471dcecc7d440b4957a5e6b560
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2021-11-27 15:14:29 +0100

    Project & Header Components ...

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import React, {Component} from "react";
 import Dashboard from "./components/Dashboard";
+import Header from "./components/layout/Header";
 
 class App extends Component{
 
   render() {
     return (
         <div className="App">
+            <Header/>
             <Dashboard/>
         </div>
     );
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,9 +1,16 @@
 import React, {Component} from 'react';
+import ProjectItem from "./project/ProjectItem";
+
 
 class Dashboard extends Component {
     render() {
         return (
-            <h1>Welcome to the Dashboard</h1>
+            <div>
+                <h1>Welcome to the Dashboard</h1>
+                <ProjectItem/>
+                <ProjectItem/>
+                <ProjectItem/>
+            </div>
         );
     }
 }
