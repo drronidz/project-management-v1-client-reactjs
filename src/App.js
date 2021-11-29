1fbb04e4855f8d6a315176d645c12566aa3c43be
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2021-11-29 22:47:22 +0100

    Create Redux Store ...

--- src/App.js
+++ src/App.js
@@ -6,18 +6,22 @@ import Dashboard from "./components/Dashboard";
 import Header from "./components/layout/Header";
 import {BrowserRouter, Route} from "react-router-dom";
 import AddProject from "./components/project/AddProject";
+import {Provider} from "react-redux"
+import store from "./store/store";
 
 class App extends Component{
 
   render() {
     return (
-        <BrowserRouter>
-            <div className="App">
-                <Header/>
-                <Route exact path="/dashboard" component={Dashboard}/>
-                <Route exact path="/addProject" component={AddProject}/>
-            </div>
-        </BrowserRouter>
+        <Provider store={store}>
+            <BrowserRouter>
+                <div className="App">
+                    <Header/>
+                    <Route exact path="/dashboard" component={Dashboard}/>
+                    <Route exact path="/addProject" component={AddProject}/>
+                </div>
+            </BrowserRouter>
+        </Provider>
     );
   }
 
