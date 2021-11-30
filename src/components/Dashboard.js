5a295ab3ceeb260b12c3feb322600c6e9b465f1b
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2021-11-30 23:37:18 +0100

    Get Projects (BackSide REDUX) ...

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,9 +1,16 @@
 import React, {Component} from 'react';
 import ProjectItem from "./project/ProjectItem";
 import CreateProjectButton from "./project/CreateProjectButton";
+import { connect } from "react-redux"
+import PropTypes from "prop-types";
+import { getProjects } from "../actions/projectActions";
 
 
 class Dashboard extends Component {
+    componentDidMount() {
+        this.props.getProjects()
+    }
+
     render() {
         return (
             <div className="projects">
@@ -24,4 +31,16 @@ class Dashboard extends Component {
     }
 }
 
-export default Dashboard;
+Dashboard.propTypes = {
+    project: PropTypes.object.isRequired,
+    getProjects: PropTypes.func.isRequired
+}
+
+const mapStateToProps = state => ({
+    project: state.project
+})
+
+export default connect(
+    mapStateToProps,
+    { getProjects })
+(Dashboard);
