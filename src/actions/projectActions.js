4ed43bb73edf00d36e6f56b4f845a438145db140
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2021-12-01 14:23:38 +0100

    Get project by ID, Update Use case ...

--- src/actions/projectActions.js
+++ src/actions/projectActions.js
@@ -1,5 +1,5 @@
 import axios from "axios"
-import {GET_ERRORS, GET_PROJECTS} from "./types";
+import {GET_ERRORS, GET_PROJECT, GET_PROJECTS} from "./types";
 
 export const createProject = (project, history) => async dispatch => {
     try {
@@ -18,6 +18,14 @@ export const getProjects = () => async dispatch => {
     const response = await axios.get("http://localhost:8080/api/project/all")
     dispatch({
         type: GET_PROJECTS,
+        payload: response.data
+    })
+}
+
+export const getProject = (id, history) => async dispatch => {
+    const response = await axios.get(`http://localhost:8080/api/project/${id}`)
+    dispatch({
+        type: GET_PROJECT,
         payload: response.data
     })
 }
